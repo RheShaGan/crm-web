05004f8206437d787330de2616fb58f4ff2b81a9
RHEA GANAPATHY <rheasg@gmail.com>
2015-06-18 17:06:59 -0400

    Handling put form submission

--- crm.rb
+++ crm.rb
@@ -30,9 +30,32 @@ get "/contacts/:id" do
   end
 end
 
+get "/contacts/:id/edit" do
+  @contact = $rolodex.search_contact(params[:id].to_i)
+  if @contact
+      erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 # end of file
 post '/contacts' do
   #new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
   $rolodex.add_contact(params[:first_name], params[:last_name], params[:email], params[:note])
   redirect to('/contacts')
+end
+
+put "/contacts/:id" do
+  @contact = $rolodex.search_contact(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.notes = params[:notes]
+
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
 end
