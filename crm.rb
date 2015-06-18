220d9183203af5e52cc361065d9906662f170c34
RHEA GANAPATHY <rheasg@gmail.com>
2015-06-18 16:19:33 -0400

    search contact updated

--- crm.rb
+++ crm.rb
@@ -21,6 +21,15 @@ get "/contacts/new" do
   erb :new_contact
 end
 
+get "/contacts/:id" do
+  @contact = $rolodex.search_contact(params[:id].to_i)
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 # end of file
 post '/contacts' do
   #new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
